98921a239dee5e442ce9ab344f0015aea619518e
nikhilkumar <nikhilkumar@expediagroup.com>
2024-04-03 02:38:56 +0530

    Day 8 Submissions
    Signed-off-by: nikhilkumar <nikhilkumar@expediagroup.com>

---
+++ nikhil/LC/subarrays_with_k_different_integers.cpp
@@ -0,0 +1,129 @@
+// https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+
+#include <bits/stdc++.h>
+
+#define endl "\n"
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define PII pair<int, int>
+#define MAX 1000005
+#define inf 1000000000000000LL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+typedef long long ll;
+
+// auto it = [&]() {
+// cout<<"Nikhil"
+// };
+// it();
+
+template <typename T>
+string to__string(T x)
+{
+    ostringstream ss;
+    ss << x;
+    return ss.str();
+}
+
+int fast_expo(ll a, ll b, ll p) // usually p is 10^9 + 7(prime)
+{
+    if (a == 0)
+        return 0;
+
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans *= a, ans %= p;
+        a *= a, a %= p;
+        b /= 2;
+    }
+    return ans;
+}
+
+ll inv(ll a, ll p) // modulo inverse signifies a value "b" such that a*b = 1(mod p)
+{
+    return fast_expo(a, p - 2, p);
+}
+
+/* ll nCk(ll n, ll k, ll p){
+    return ((fact[n] * inv(fact[k],p)%p) * inv(fact[n-k],p)) % p;
+} */
+
+ll max(ll a, ll b)
+{
+    if (a >= b)
+        return a;
+    return b;
+}
+
+ll min(ll a, ll b)
+{
+    if (a >= b)
+        return b;
+    return a;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+/*----------GLOBAL BOIS--------------- */
+
+class Solution
+{
+public:
+    int solve(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        unordered_map<int, int> hash;
+
+        int left = 0, diff_cnt = 0, res = 0;
+
+        for (int right = 0; right < n; right++)
+        {
+            hash[nums[right]]++;
+
+            while (hash.size() > k)
+            { // window size  > k
+                hash[nums[left]]--;
+                if (hash[nums[left]] == 0)
+                    hash.erase(nums[left]);
+                left++;
+            }
+            res += right - left + 1;
+        }
+        return res;
+    }
+
+    int subarraysWithKDistinct(vector<int> &nums, int k)
+    {
+        return solve(nums, k) - solve(nums, k - 1);
+    }
+};
+
+void solve()
+{
+}
+
+int main()
+{
+    fast_io int tc = 1;
+
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
